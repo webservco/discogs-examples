d12ea959e37de719ec9975eb92dc54e75f8f4efa
Radu Maier <ariademur@webserv.co>
2019-02-05 09:29:27 +0100

    Fix Traits/ControllerDiscogsTrait

--- src/Traits/ControllerDiscogsTrait.php
+++ src/Traits/ControllerDiscogsTrait.php
@@ -3,6 +3,10 @@ namespace Project\Traits;
 
 trait ControllerDiscogsTrait
 {
+    abstract protected function config();
+
+    abstract public function setData($key, $value);
+
     /*
     * Loads Discogs configuration array.
     */
@@ -12,7 +16,7 @@ trait ControllerDiscogsTrait
             'discogs',
             $this->config()->load(
                 'Discogs',
-                $this->data('path/project')
+                $this->config()->get('app/path/project')
             )
         );
         $this->setData('discogs/config', $this->config()->get('discogs'));
